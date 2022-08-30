71f16af873090af15ae3b88417858ce0c6730267
Andreas Sass Borggaard Madsen <andr515i@zbc.dk>
2022-08-30 17:04:27 +0200

    fixed cardstostring method, aswell as continued on the game, and am close to finish

--- Simple Blackjack/Program.cs
+++ Simple Blackjack/Program.cs
@@ -30,41 +30,86 @@ namespace Simple_Blackjack
                         cards[counter].CardColor = "Red";
                     }
                     counter++;
-                    Console.WriteLine(cards[x].CardValue);
+                    //Console.WriteLine(cards[x].CardValue);
                 }
             }
-            Cards[] player = new Cards[4];
+            Cards[] player = new Cards[5];
             Cards[] dealer = new Cards[5];
 
-
             bool run = true;  // game loop bool
-            int dealerCardCounter = 0;
+
             int playerCardCounter = 0;
+            //dealer[0] = cards[randomCard];
+            //Console.Write("\ndealer's hand has: ");
+            //for (int i = 0; i < dealer.Length; i++)        
+            //{
+            //    if (dealer[i].CardValue != 0)
+            //    {
+            //        Console.Write(CardsToString(dealer[i].CardValue));
+            //    }
+            //    else
+            //    {
+            //        Console.Write("*");
+            //    }
+            //}
+
             while (run) // game loop to make sure we can keep playing until we dont want to play anymore :)
             {
-                int randomCard = random.Next(1, 52);
-                dealer[dealerCardCounter] = cards[randomCard];
-                Console.Write("\ndealer's hand has: ");
-                for (int i = 0; i < dealer.Length; i++)
+                Console.Write(" your hand: ");
+                for (int i = 0; i < player.Length; i++)
                 {
-                    if (dealer[i].CardValue != 0)
-                    {
-                    Console.Write(dealer[i].CardValue + " ");
-                    }
-                    else
-                    {
-                        Console.Write("*");
-                    }
+                    Console.Write(CardsToString(player[i].CardValue) + " ");
                 }
-                    Console.Read();
+                int randomCard = random.Next(1, 52);
 
+                Console.WriteLine(" do you want to hit or stay? ");
 
-                dealerCardCounter++;
+                string userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case string val when (val == "hit" || val == "h"):
+                        player[playerCardCounter] = cards[randomCard];
+                        break;
+                    case string val when (val == "s" || val == "stay"):
+                        Console.Write("\ndealer's hand has: ");
+                        for (int i = 0; i < dealer.Length; i++)
+                        {
+                            randomCard = random.Next(1, 52);
+
+                            dealer[i] = cards[randomCard];
+                            if (dealer[i].CardValue != 0)
+                            {
+                                Console.Write(CardsToString(dealer[i].CardValue) + " ");
+                            }
+                            else
+                            {
+                                Console.Write("* ");
+                            }
+                        }
+                        break;
+
+                }
+
+                playerCardCounter++;
             }
-
         }
+        public static string CardsToString(int value)
+        {
 
+            switch (value)
+            {
+                case int val when val == 1 || val == 11:
+                    return "Ace";
+                case int val when val == 12:
+                    return "Jacks";
+                case int val when val == 13:
+                    return "Queens";
+                case int val when val == 14:
+                    return "Kings";
+                default:
+                    return value.ToString();
+            }
+        }
     }
-
 }
 
